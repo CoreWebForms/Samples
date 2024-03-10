6c743e1b7b8d4da1c58e4c2cc4d979e4aa4d403c
Biroj Nayak <49173255+birojnayak@users.noreply.github.com>
2024-03-10 07:02:11 +0000

    Fixing for Linux (#16)

--- samples/WingTipToys/src/Models/ProductContext.cs
+++ samples/WingTipToys/src/Models/ProductContext.cs
@@ -3,10 +3,6 @@ namespace WingtipToys.Models
 {
   public class ProductContext : DbContext
   {
-    public ProductContext()
-      : base("WingtipToys")
-    {
-    }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<CartItem> ShoppingCartItems { get; set; }
---
+++ samples/WingTipToys/src/Models/ProductDbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace WingtipToys.Models;
+
+public class ProductDbConfiguration : DbConfiguration
+{
+    public ProductDbConfiguration()
+    {
+        //https://learn.microsoft.com/en-us/dotnet/framework/data/adonet/connection-string-syntax
+        // SetDefaultConnectionFactory(new SqlConnectionFactory("Data Source=yourpublicip.opensource.com;Initial Catalog=WingtipToys;User Id=youruserid;Password=yourpassword"));
+    }
+}
